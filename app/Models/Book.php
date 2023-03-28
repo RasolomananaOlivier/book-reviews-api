a69817cc1bdb76e134da5b44ad3d998bc80dc2af
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-03-28 13:35:50 +0300

    feat: define relationship between book and rating

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -13,4 +13,9 @@ class Book extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
 }
