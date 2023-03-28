dbd077b162a0a677c10838045cc2c1231583ffdb
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-03-28 13:48:22 +0300

    feat: set up jwt

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -9,6 +9,12 @@ class Rating extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'user_id',
+        'book_id',
+        'rating',
+    ];
+
     public function book()
     {
         return $this->belongsTo(Book::class);
